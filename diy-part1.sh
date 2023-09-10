c6dd27e00be4e01a6d64c4a5337094adb11d8379
wangzan200808 <wangzan200808@sina.com>
2023-09-10 09:58:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git design https://github.com/gngpp/luci-theme-design/tree/js' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #overwall测试版
 #sed -i '$a src-git overwall https://github.com/wangzan200808/overwall' feeds.conf.default
